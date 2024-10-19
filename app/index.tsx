3aeda281adb76742d88e2f42368b786c857605df
wael <wael.22@live.fr>
2024-10-19 11:36:09 +0100

    routing

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { FlatList, Text, View } from "react-native";
+import products from "../assets/products.json";
+import ProductListItem from "../components/ProductListItem";
+
+
+
+export default function HomeScreen(){
+  return (
+      <FlatList data={products} renderItem={({item})=>(
+        <ProductListItem product={item} />
+      )} />
+  )
+}
---
+++ components/ProductListItem.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ProductListItem({product}: any){
+  return (
+    <View>
+      <Text>{product.name}</Text>
+    </View>
+  )
+}
